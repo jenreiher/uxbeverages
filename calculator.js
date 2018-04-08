8713ddf03081ebc6ee8289d27bf91adbbe16f685
jen <jen@reiher.ca>
2018-04-07 20:17:03 -0700

    Update to calculate both buying and drinking with dynamic lists of options

--- calculator.js
+++ calculator.js
@@ -2,31 +2,217 @@ new Vue({
   el: '#app',
   data: {
     quantity: "1",
-    volume: 355,
+    volumeDrinking: "na",
+    volumeBuying: "na",
     type: "beer",
-    total: 1,
-  },
-   methods: {
-    calculate: function () {
-      if (!this.quantity && !this.volume && !this.type) {
-        alert("Quantity, Volume and Type are required");
-      } else {
-        // Translate the string values to integers
-        let quantity = parseInt(this.quantity, 10);
-
-        // Get the value of a beverage type
-        let value = this.calcValue(this.type);
-
-        // Since the minimum can of beer you can buy is 355, let the be the standard
-        // More volume = higher standardize amount
-        let standardizeAmt = (quantity * this.volume)/355;
-
-        // Worth is a function of a beverages value multipled by its standardized amount
-        let worth = Math.round(value * standardizeAmt);
-
-        return this.total = worth;
-      }
+    drinkOptions: {
+      beer: [
+        {
+          name: 'canSm',
+          description: 'Small Can',
+          oz: 12,
+          mL: 355
+        },
+        {
+          name: 'canLg',
+          description: 'Large Can',
+          oz: 16,
+          mL: 473
+        },
+        {
+          name: 'btlSm',
+          description: 'Small Bottle',
+          oz: 16,
+          mL: 473
+        },
+        {
+          name: 'btlLg',
+          description: 'Large Bottle',
+          oz: 21,
+          mL: 650
+        },
+      ],
+      cider: [
+        {
+          name: 'canSm',
+          description: 'Small Can',
+          oz: 12,
+          mL: 355
+        },
+        {
+          name: 'canLg',
+          description: 'Large Can',
+          oz: 16,
+          mL: 473
+        },
+        {
+          name: 'btlSm',
+          description: 'Small Bottle',
+          oz: 16,
+          mL: 473
+        }
+      ],
+      wine: [
+        {
+          name: 'glass',
+          description: 'Large Glass',
+          oz: 9,
+          mL: 255
+        }
+      ],
+      spirit: [
+        {
+          name: 'shot',
+          description: '2oz shot',
+          oz: 2,
+          mL: 60
+        }
+      ],
+      highball: [
+       {
+          name: 'glassShort',
+          description: 'Short Glass',
+          oz: 8,
+          mL: 235
+        },
+      ],
+      nonalcoholic: [
+        {
+          name: 'canSm',
+          description: 'Small Can',
+          oz: 12,
+          mL: 355
+        },
+        {
+          name: 'canLg',
+          description: 'Large Can',
+          oz: 16,
+          mL: 473
+        },
+        {
+          name: 'btlSm',
+          description: 'Small Bottle',
+          oz: 16,
+          mL: 473
+        },
+        {
+          name: 'btlLg',
+          description: 'Large Bottle',
+          oz: 21,
+          mL: 650
+        },
+      ],
     },
+    buyOptions: {
+      beer: [
+        {
+          name: 'canSm',
+          description: 'Small Can',
+          oz: 12,
+          mL: 355
+        },
+        {
+          name: 'canLg',
+          description: 'Large Can',
+          oz: 16,
+          mL: 473
+        },
+        {
+          name: 'btlSm',
+          description: 'Small Bottle',
+          oz: 16,
+          mL: 473
+        },
+        {
+          name: 'btlLg',
+          description: 'Large Bottle',
+          oz: 21,
+          mL: 650
+        },
+      ],
+      cider: [
+        {
+          name: 'canSm',
+          description: 'Small Can',
+          oz: 12,
+          mL: 355
+        },
+        {
+          name: 'canLg',
+          description: 'Large Can',
+          oz: 16,
+          mL: 473
+        },
+        {
+          name: 'btlSm',
+          description: 'Small Bottle',
+          oz: 16,
+          mL: 473
+        },
+        {
+          name: 'btlMd',
+          description: 'Medium Bottle',
+          oz: 21,
+          mL: 650
+        },
+        {
+          name: 'btlLg',
+          description: 'Large Bottle',
+          oz: 32,
+          mL: 750
+        },
+      ],
+      wine: [
+        {
+          name: 'bottleLg',
+          description: 'Large Bottle',
+          oz: 32,
+          mL: 750
+        },
+      ],
+      spirit: [
+        {
+          name: 'btlMd',
+          description: 'Medium Bottle',
+          oz: 21,
+          mL: 650
+        }
+      ],
+      nonalcoholic: [
+        {
+          name: 'canSm',
+          description: 'Small Can',
+          oz: 12,
+          mL: 355
+        },
+        {
+          name: 'canLg',
+          description: 'Large Can',
+          oz: 16,
+          mL: 473
+        },
+        {
+          name: 'btlSm',
+          description: 'Small Bottle',
+          oz: 12,
+          mL: 355
+        },
+        {
+          name: 'btlMd',
+          description: 'Medium Bottle',
+          oz: 16,
+          mL: 473
+        },
+        {
+          name: 'btlLg',
+          description: 'Large Bottle',
+          oz: 21,
+          mL: 650
+        },
+      ],
+    },
+  },
+  methods: {
     calcValue: function(type) {
       // 1 is the default. Only adjust if its value is not this.
       let value = 1;
@@ -41,12 +227,54 @@ new Vue({
         value = 2;
       }
 
-      // High value here because the drunk factor is like whoa
+      // A highball is worth a combination of spirit and non-alcoholic
+      if (type === 'highball') {
+        value = 2.5;
+      }
+
+      // High value here because the drunk factor is like whoa but the volume is like nothing
       if (type === 'spirit') {
-        value = 5;
+        value = 10;
       }
 
       return value;
+    },
+  },
+  computed: {
+    calculate: function () {
+      let value = 1;
+      let quantityBuying = 0;
+      let standardizeAmtBuying = 0;
+      let standardizeAmtDrinking = 0;
+      let worth = 1;
+
+      // Get the value of a beverage type
+      value = this.calcValue(this.type);
+
+      // Translate the string values to integers
+      quantityBuying = parseInt(this.quantity, 10);
+
+      if (this.volumeBuying != "na") {
+        // Since the minimum can of beer you can buy is 355, let the be the standard
+        // More volume = higher standardize amount
+        standardizeAmtBuying = (quantityBuying * this.volumeBuying)/355;
+      }
+
+      if (this.volumeDrinking != "na") {
+        // Since the minimum can of beer you can buy is 355, let the be the standard
+        // More volume = higher standardize amount
+        standardizeAmtDrinking = this.volumeDrinking/355;
+      }
+
+      // Worth is a function of a beverages value multipled by its standardized amount
+      worth = Math.round((value * standardizeAmtBuying) - (value * standardizeAmtDrinking));
+
+console.log("value:", value);
+console.log("buying:", quantityBuying);
+console.log("std buying:", standardizeAmtBuying);
+console.log("std drinking:", standardizeAmtDrinking);
+console.log("worth:", worth)
+      return worth;
     }
   },
-})
+});
